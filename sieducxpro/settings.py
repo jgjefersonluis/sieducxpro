a3cc3da04457ed01012550c951af07979297b48e
Jeferson Luis <jgjefersonluis@gmail.com>
2019-11-07 00:22:26 -0300

    2

--- sieducxpro/settings.py
+++ sieducxpro/settings.py
@@ -14,9 +14,6 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
-STATIC_DIR = os.path.join(BASE_DIR, 'static')
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
@@ -125,7 +122,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-LOGOUT_REDIRECT_URL = 'base'
+
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
